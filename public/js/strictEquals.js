53637fca6d0038a4982436a14e0cb45d6c1581a5
oriolraventosc <oriol.raventos.c@gmail.com>
2022-10-04 22:55:18 +0200

    Solve bugfix of the third child of the function

--- public/js/strictEquals.js
+++ public/js/strictEquals.js
@@ -1,9 +1,6 @@
 export const strictEquals = (a, b) => {
   let respuesta;
-  if (Object.is(Number.isNaN(a, b))) {
-    respuesta = false;
-    return respuesta;
-  } else if (Object.is(a, b)) {
+  if (Object.is(a, b)) {
     respuesta = true;
     return respuesta;
   } else if (Object.is(a, -0) && Object.is(0, b)) {
@@ -11,6 +8,9 @@ export const strictEquals = (a, b) => {
     return respuesta;
   } else if (Object.is(0, a) && Object.is(b, -0)) {
     respuesta = true;
+    return respuesta;
+  } else if (Object.is(NaN, NaN)) {
+    respuesta = false;
     return respuesta;
   } else {
     respuesta = false;
